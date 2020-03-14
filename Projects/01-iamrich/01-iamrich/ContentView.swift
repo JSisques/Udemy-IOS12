e95d57c49ef958ffa6ca274af0d9809dc0c3b2a5
Javi <javierplazasisques@gmail.com>
2020-03-14 21:42:01 +0100

    Root commit

---
+++ Projects/01-iamrich/01-iamrich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  01-iamrich
+//
+//  Created by Javier Plaza Sisqués on 14/03/2020.
+//  Copyright © 2020 Javier Plaza Sisqués. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
