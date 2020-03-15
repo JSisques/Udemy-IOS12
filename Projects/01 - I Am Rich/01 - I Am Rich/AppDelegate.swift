78f08963a534d5d251218e73b034f1ad0838ee81
Javi <javierplazasisques@gmail.com>
2020-03-15 14:56:18 +0100

    15/03/2020

--- Projects/01 - I Am Rich/01 - I Am Rich/AppDelegate.swift
+++ Projects/01 - I Am Rich/01 - I Am Rich/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  01 - I Am Rich
 //
-//  Created by Javier Plaza Sisqués on 14/03/2020.
+//  Created by Javier Plaza Sisqués on 15/03/2020.
 //  Copyright © 2020 Javier Plaza Sisqués. All rights reserved.
 //
 
---
+++ Projects/01 - I Am Rich/01 - I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01 - I Am Rich
+//
+//  Created by Javier Plaza Sisqués on 15/03/2020.
+//  Copyright © 2020 Javier Plaza Sisqués. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
